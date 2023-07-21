b05efc66c2be6383572a872e764eff4ed2cb9874
Aryan <ap805203@gmail.com>
2023-07-21 15:54:53 +0530

    initial commit

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -12,7 +12,7 @@ import {Link} from "react-router-dom"
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <Link class="nav-link active " aria-current="page" to="/">Hom</Link>
+          <Link class="nav-link active " aria-current="page" to="/">Home</Link>
         </li>
         <li class="nav-item">
           <Link class="nav-link " to="/">About</Link>
